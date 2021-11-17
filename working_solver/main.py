df2707f54b1711b6f18fd31426b6ab327cdfa750
Robin Stöhr <robinbux@gmail.com>
2021-11-17 13:34:48 +0100

    WORKING

--- working_solver/main.py
+++ working_solver/main.py
@@ -1,76 +1,153 @@
 import copy
 from rules import Rules
-
+import numpy as np
 # 9x9
-"sudoku-rules.txt"
-"sudoku-example.txt"
+#"sudoku-rules.txt"
+#"sudoku-example.txt"
 
-SUDOKU_RULES_FILEPATH = "sudoku-rules-4x4.txt"
-SUDOKU_EXAMPLE_FILEPATH = "sudoku-example4.txt"
+#SUDOKU_RULES_FILEPATH = "sudoku-rules-4x4.txt"
+#SUDOKU_EXAMPLE_FILEPATH = "sudoku-example4.txt"
+
+SUDOKU_RULES_FILEPATH = "sudoku-rules.txt"
+SUDOKU_EXAMPLE_FILEPATH = "sudoku-example.txt"
 
 def read_example(filepath: str):
     with open(filepath) as f:
         lines = f.readlines()
         return [clause[:-3] for clause in lines]
 
+
 idx = 0
 true_literals = []
+literals_added = []
 
+print("TEEEEEsT")
 def dpll_2(rules: Rules, literal: str) -> bool:
     global idx
     global true_literals
     print(f"-- New DPLL idx: {idx}")
     idx = idx + 1
-    # Remove clauses from α containing literal
-    # Shorten clauses from α containing -literal
-    rules.remove_or_shorten_clauses_containing_literal(literal=literal)
+
+    #clauses_before = rules.clauses_containing_three_three_literals()
+    #rules.remove_or_shorten_clauses_containing_literal(literal=literal)
+    clauses_before_remove = rules.clauses_containing_three_three_literals()
+
+    rules.remove_clause(literal)
+
+    clauses_before_shorten = rules.clauses_containing_three_three_literals()
+
+    rules.shorten_clause(literal)
+
+    clauses_after_shorten = rules.clauses_containing_three_three_literals()
+
 
     if not rules.contains_clauses():
         true_literals.append(literal)
+        print("SULUTION FOUND")
         return True
     if rules.contains_empty_clauses():
-        print("-------EMPTY CLAUSES ------------")
-        #true_literals = true_literals[:-len(unit_list)]
+        #print("-------EMPTY CLAUSES ------------")
+        # true_literals = true_literals[:-len(unit_list)]
+        #true_literals.pop()
+        print("RETURN FALSE IN EMPTY CLAUSE")
         return False
 
     # DO UNIT RULE
-    rules.unit_rule()
+    unit_rule_literals = rules.unit_rule()
+    for lit in unit_rule_literals:
+        rules.remove_clause(lit)
+        rules.shorten_clause(lit)
+        if lit in rules._literals:
+            rules._literals.remove(lit)
 
-    #for uliterals in unit_rule_literals:
-        #neg_uliteral = f"-{uliterals}" if not "-" in uliterals else uliterals[1:]
-        #if neg_uliteral in unit_rule_literals or neg_uliteral in true_literals:
-        #    return False
-        #true_literals.append(uliterals.replace("-", ""))
+        neg = literal[1:] if '-' in literal else f'-{literal}'
+        if neg in rules._literals:
+            rules._literals.remove(neg)
+        if '-' not in lit:
+            pass
+            true_literals.append(lit)
 
 
-    # next_literal = rules.pick_random_literal()
+    if not rules.contains_clauses():
+        true_literals.append(literal)
+        print("SULUTION FOUND")
+        return True
+    if rules.contains_empty_clauses():
+        #print("-------EMPTY CLAUSES ------------")
+        # true_literals = true_literals[:-len(unit_list)]
+        #true_literals.pop()
+        print("RETURN FALSE IN EMPTY CLAUSE")
+        # TODO: REMOVE fRoM TRUE unit
+        for lit in unit_rule_literals:
+            if '-' not in lit:
+                true_literals.remove(lit)
+        return False
+
+
     rules.jerslow_wang_heuristic()
+
     next_literal = rules.get_literal()
+    #next_literal = rules.pick_random_literal()
+
 
     neg_next_literal = f"-{next_literal}" if not "-" in next_literal else next_literal[1:]
+
     if dpll_2(rules=copy.deepcopy(rules), literal=neg_next_literal):
-        true_literals.append(literal)
+        true_literals.append(neg_next_literal)
         return True
     temp_result = dpll_2(rules=copy.deepcopy(rules), literal=next_literal)
     if temp_result:
-        true_literals.append(literal)
+        #print("-------Return TRUE ------------")
+        true_literals.append(next_literal)
+        pass
+    else:
+        #print("-------Return FAlSE ------------")
+        #true_literals.append(next_literal)
+        for lit in unit_rule_literals:
+            if '-' not in lit:
+                true_literals.remove(lit)
+        pass
     return temp_result
+
 
 sudoku = read_example(SUDOKU_EXAMPLE_FILEPATH)
 rules = Rules(filepath=SUDOKU_RULES_FILEPATH)
 
 for literal in sudoku:
-    rules.remove_or_shorten_clauses_containing_literal(literal=literal)
+    #rules.remove_or_shorten_clauses_containing_literal(literal=literal)
+    rules.remove_clause(literal)
+    rules.shorten_clause(literal)
 
 rules.jerslow_wang_heuristic()
-    
+
 initial_literal = rules.get_literal()
 neg_initial_literal = f"-{initial_literal}" if not "-" in initial_literal else initial_literal[1:]
 result = dpll_2(rules=copy.deepcopy(rules), literal=neg_initial_literal)
 if not result:
-    result = dpll_2(rules=rules, literal=initial_literal)
+    result = dpll_2(rules=copy.deepcopy(rules), literal=initial_literal)
 
 true_true_literals = set([lit for lit in true_literals if "-" not in lit])
+three_three_literals = set([lit for lit in true_literals if "-" not in lit and lit.startswith("33")])
+print(three_three_literals)
 print(true_true_literals)
 print(len(true_true_literals))
 print(result)
+
+# for uliterals in unit_rule_literals:
+# neg_uliteral = f"-{uliterals}" if not "-" in uliterals else uliterals[1:]
+# if neg_uliteral in unit_rule_literals or neg_uliteral in true_literals:
+#    return False
+# true_literals.append(uliterals.replace("-", ""))
+
+solution = list(true_true_literals) + sudoku
+s=int(max(solution)[0])
+size=(s,s)
+matrix =np.zeros(size)
+
+for sol in solution:
+    row = int(sol[0])-1
+    col = int(sol[1])-1
+    num = int(sol[2])
+    matrix[row][col] = num
+
+print(matrix)
--- working_solver/rules.py
+++ working_solver/rules.py
@@ -1,14 +1,27 @@
 import random
 import copy
 
+
 class Rules:
 
     def __init__(self, filepath: str):
+        self._filepath = filepath
         self._clauses: list[str] = []
         self._literals = set()
         self._read_rules(filepath=filepath)
-        self._literal_dict = dict(zip(copy.copy(self._literals),[0]*len(self._literals)))
+        self._literal_dict = dict(zip(copy.copy(self._literals), [0] * len(self._literals)))
         self.jerslow_wang_heuristic()
+        self.three_three_clauses = []
+
+    def clauses_containing_three_three_literals(self):
+        clauses = []
+        for clause in self._clauses:
+            for lit in clause.split():
+                if lit.replace("-", "").startswith("33"):
+                    clauses.append(clause)
+                    break
+        self.three_three_clauses = clauses
+        return clauses
 
     def _read_rules(self, filepath: str):
         with open(filepath) as f:
@@ -20,7 +33,34 @@ class Rules:
                     self._literals.add(variable)
                     self._literals.add(f"-{variable}")
             print(f"Length of variables: {len(self._literals)}")
-            
+
+    def _reset_literals(self):
+        self._literals = set()
+        for clause in self._clauses:
+            for literal in clause.split():
+                variable = literal.replace("-", "")
+                self._literals.add(variable)
+                self._literals.add(f"-{variable}")
+        self._literal_dict = dict(zip(copy.copy(self._literals), [0] * len(self._literals)))
+
+    def remove_clause(self, literal: str) -> None:
+        for clause in reversed(self._clauses):
+            if literal in clause.split():
+                self._clauses.remove(clause)
+
+    def shorten_clause(self, literal: str) -> None:
+        before = self.clauses_containing_three_three_literals()
+        variable = literal[1:] if '-' in literal else f'-{literal}'
+        for i in range(len(self._clauses) - 1, -1, -1):
+            if variable in self._clauses[i].split():
+                #print(f"Clause before: {self._clauses[i]}")
+                before_reassign = self.clauses_containing_three_three_literals()
+                self._clauses[i] = self._clauses[i].replace(variable, "").strip()
+                after_reassign = self.clauses_containing_three_three_literals()
+                #print(f"Clause after: {self._clauses[i]}")
+        after = self.clauses_containing_three_three_literals()
+        x = 5
+
     def remove_or_shorten_clauses_containing_literal(self, literal: str):
         # "111"  "-112"
         variable = literal.replace("-", "")
@@ -42,17 +82,25 @@ class Rules:
                     else:
                         # TODO: Remove whole claiuse
                         del self._clauses[i]
-                        
+
     def contains_clauses(self):
         return len(self._clauses) > 0
-    
+
     def contains_empty_clauses(self):
-        for idx, clause in enumerate(self._clauses):
-            if not clause:
+        for clause in self._clauses:
+            if not clause.strip():
                 return True
         return False
 
     def unit_rule(self):
+        literals = set()
+        for clause in self._clauses:
+            clause_literals = clause.split()
+            if len(clause_literals) == 1:
+                literals.add(clause_literals[0])
+        return literals
+
+        """
         literals = []
         for clause in reversed(self._clauses):
             clause_literals = clause.split()
@@ -60,41 +108,28 @@ class Rules:
                 literals.extend(clause_literals)
                 self._clauses.remove(clause)
                 literal = clause_literals[0]
-                #neg_literal = f"-{literal}" if not "-" in literal else literal[1:]
-                for i in range(len(self._clauses) - 1, -1, -1):
-                    clause_list = self._clauses[i].split()
-                    if literal in clause_list:
-                        if '-' in literal:
-                            if literal in clause_list:
-                                # TODO: Remove whole clause
-                                del self._clauses[i]
-                            else:
-                                # TODO: REMOVE LITER
-                                self._clauses[i] = self._clauses[i].replace(literal, "").strip()
-                        # Positive literal
-                        else:
-                            if f"-{literal}" in clause_list:
-                                # TODO: REMOVE LITER
-                                self._clauses[i] = self._clauses[i].replace(f"-{literal}", "").strip()
-                            else:
-                                # TODO: Remove whole clause
-                                del self._clauses[i]
-
-
+                negation = literal[1:] if '-' in literal else f'-{literal}'
+                # neg_literal = f"-{literal}" if not "-" in literal else literal[1:]
         return literals
-    
+        """
+
     def pick_random_literal(self):
-        literal = random.sample(self._literals, 1)[0]
-        self._literals.remove(literal)
+        #self._reset_literals()
+        #literal = random.sample(self._literals, 1)[0]
+        #self._literals.remove(literal)
+        literal = self._clauses[0].split()[0]
         return literal
 
     def jerslow_wang_heuristic(self):
+        #self._reset_literals()
+        #print(len(self._literals))
         self._literal_dict = dict(zip(copy.copy(self._literals), [0] * len(self._literals)))
         for clause in self._clauses:
             for literal in clause.split():
-                self._literal_dict[literal] = self._literal_dict[literal] + 2**(-len(clause.split()))
+                self._literal_dict[literal] = self._literal_dict[literal] + 2 ** (-len(clause.split()))
 
     def get_literal(self):
+
         max_literal = max(self._literal_dict, key=self._literal_dict.get)
         neg_max_literal = f"-{max_literal}" if not "-" in max_literal else max_literal[1:]
 
